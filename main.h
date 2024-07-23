090b0ed35bf5e2b7a977493bb8abb3d7c4529f4a
Melvin Redondo--Tanis <melvin@redondotanis.com>
2024-07-23 11:46:27 +0200

    add struct for pointer functions and remove stupid things

--- main.h
+++ main.h
@@ -5,6 +5,18 @@
 #include <stdlib.h>
 #include <stdarg.h>
 
+typedef struct
+{
+	char spec;
+	void (*func)(va_list *args);
+} format_spec;
+
+int put_c(va_list *args);
+int put_s(va_list *args);
+int put_d(va_list *args);
+int put_i(va_list *args);
+int put_prct(va_list *args);
+
 int _printf(const char *format, ...);
 
 #endif
